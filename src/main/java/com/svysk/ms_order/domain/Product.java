732f3edc1ba9285c4f00f4972395fc3b76127272
Svitlana Vyskrebentseva <vyskrebentseva.svetlana@gmail.com>
2025-01-13 13:58:38 +0200

    Refactor Cart logic

--- src/main/java/com/svysk/ms_order/domain/Product.java
+++ src/main/java/com/svysk/ms_order/domain/Product.java
@@ -2,10 +2,25 @@ package com.svysk.ms_order.domain;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Product {
 
     Long id;
     String name;
     Double price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/com/svysk/ms_order/domain/repository/CartRepository.java
+++ src/main/java/com/svysk/ms_order/domain/repository/CartRepository.java
@@ -7,9 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository {
-    List<Cart> findCartByUserId(String userId);
-
-    Optional<Cart> findCartByUserIdAndProduct(String userId, Product product);
+    Optional<Cart> findByUser(String userId);
 
     Optional<Cart> save(Cart cart);
 
